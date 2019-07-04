4c2a7e0b710eea0ae047468da1b9708d0daa5a31
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-04 13:28:15 -0300

    criado tema para o projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ola mundo</h1>
-    </div>
-  );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5', 
+    },
+    secondary: {
+      main: '#631976',
+    },
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <h1>Hello world</h1>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
 }
 
 export default App;
