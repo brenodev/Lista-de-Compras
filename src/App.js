91f40b7c2207d4145216950b08f09ce2e8536925
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-04 13:45:46 -0300

    configurado font e alguns padroes do projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar';
 import './App.css';
 
 const theme = createMuiTheme({
@@ -18,8 +19,13 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <AppBar position='static' color='primary'>
+          <div className='header'>
+            <p>Lista de Compras</p>
+          </div>
+        </AppBar>
         <div>
-          <h1>Hello world</h1>
+          <h1></h1>
         </div>
       </MuiThemeProvider>
     )
