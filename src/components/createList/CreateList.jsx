d2c64514b002bba27acb0ebf8b6c02b74f822dd4
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-05 14:31:44 -0300

    adicionado formulario no componente CreateList

--- src/components/createList/CreateList.jsx
+++ src/components/createList/CreateList.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import { InputAdornment } from "@material-ui/core";
 
+const units = [
+  'kg', 'lt', 'un'
+]
 
 const CreateList = () => (
-  <div>
-    <h1>Create List</h1>
+  <div className="page-container">
+    <form action="" className="form-container">
+      <TextField
+        value={""}
+        name="list"
+        label="Lista"
+        className=""
+        onChange={() => {}}
+        required
+      />
+      <TextField
+        value={""}
+        name="product"
+        label="Produto"
+        className=""
+        onChange={() => {}}
+        required
+      />
+      <TextField
+        value={""}
+        name="quantity"
+        label="Quantidade"
+        className=""
+        onChange={() => {}}
+        required
+      />
+      <TextField
+        select
+        value={""}
+        name="unity"
+        label="Unidade"
+        className=""
+        onChange={() => {}}
+        required
+      >
+        {
+          units.map(option => (
+            <MenuItem key={option} value={option}>{option}</MenuItem>
+          ))}
+      </TextField>
+      <TextField
+        value={""}
+        name="price"
+        label="Preço"
+        className=""
+        onChange={() => {}}
+        InputProps={{
+          startAdornment: <InputAdornment position='start'>R$</InputAdornment>
+        }}
+      />
+    </form>
   </div>
-)
+);
 
-export default CreateList
+export default CreateList;
