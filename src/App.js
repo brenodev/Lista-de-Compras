6137547e6be79db97a1d44e89adbe602a7bb2d64
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-05 16:53:03 -0300

    adicionando redux no projeto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 // STYLE
 import './App.css';
@@ -9,7 +10,8 @@ import './App.css';
 // CUSTOM COMPONENTE
 import CreateList from './components/createList'
 import Header from './common/header'
-import Home from  './common/home'
+import Home from './common/home'
+import store from './store'
 
 
 
@@ -19,7 +21,7 @@ const theme = createMuiTheme({
   },
   palette: {
     primary: {
-      main: '#3f51b5', 
+      main: '#3f51b5',
     },
     secondary: {
       main: '#631976',
@@ -30,18 +32,21 @@ const theme = createMuiTheme({
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <Router>
-          <>
-          <Header />
-          <Switch>
-            <Route exact path='/' component={Home}  />
-            <Route path='/Lista' component={CreateList}  />
-          </Switch>
-          </>
+      <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+          <Router>
+            <>
+              <Header />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/Lista' component={CreateList} />
+              </Switch>
+            </>
 
-        </Router>
-      </MuiThemeProvider>
+          </Router>
+        </MuiThemeProvider>
+      </Provider>
+
     )
   }
 }
