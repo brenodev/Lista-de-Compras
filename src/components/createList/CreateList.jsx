74febb2b603406a13d915a83756ade844c052a21
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-08 09:33:34 -0300

    conectando componente createList

--- src/components/createList/CreateList.jsx
+++ src/components/createList/CreateList.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 // STYLE
-import './CreateList.css'
+import "./CreateList.css";
 
 //CUSTOM COMPONENT
 import Form from "../../common/forms";
-import ListItemCard from '../listItemcard'
+import ListItemCard from "../listItemcard";
+import { Creators as ListActions } from "../../store/actions/List";
+
+class CreateList extends Component {
+
+  addProduct = (product, list) => {
+    this.props.addProduct(product, list)
+  }
 
 
-const CreateList = () => (
-  <div className="page-container">
-    <Form />
+  render() {
+    return (
+      <div className="page-container">
+        <Form addProduct={this.addProduct} />
 
-    <div className="list-items-container">
-      <ListItemCard />
-    </div>
-  </div>
-);
+        <div className="list-items-container">
+          <ListItemCard />
+        </div>
+      </div>
+    );
+  }
+}
 
-export default CreateList;
+const mapStateToProps = state => ({
+  list: state.list
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(ListActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateList);
